b9befaa303247c838722548aaab22dc584774fc5
two-pack <twopackas@gmail.com>
2019-08-13 12:54:14 +0900

    Fixes to get enum value.

--- app/models/morale.rb
+++ app/models/morale.rb
@@ -12,7 +12,7 @@ class Morale < Feeling
   def level
     return nil unless @feelings
     return level_name(@cached_level) if @cached_level
-    average = @feelings.inject(0) {|result, feeling| result += feeling[:level]} / @feelings.size.to_f
+    average = @feelings.inject(0) {|result, feeling| result += feeling.level_before_type_cast} / @feelings.size.to_f
     count =  Feeling.levels.length
     1.upto(count) do |index|
       if average <= ((count - 1) / count.to_f) * index
